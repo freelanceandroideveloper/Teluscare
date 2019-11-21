afebfa1f6760434bb727032dbcfa818817260fce
sandeep <sandeepry92@gmail.com>
2019-11-21 13:15:15 +0530

    SplashScreen testing with svg

---
+++ app/src/main/java/com/teluscare/android/view/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.teluscare.android.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Created by SandeepY on 21112019
+ **/
+
+
+public class SplashActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
